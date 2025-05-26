ff2b597068d7f3da0f5c4d411773e028cb621108
Kalvis Apsitis <kalvis.apsitis@zimperium.com>
2025-05-27 01:55:27 +0300

    uncomment py commands

--- Jenkins/ws-minimal.groovy
+++ Jenkins/ws-minimal.groovy
@@ -51,13 +51,13 @@ pipeline {
                     cp ${CONFIG_DIR}/.env .
                     ${env.PYTHON_PATH} eliozo_client.py create-task --query query.txt --reference ../tests/master-demo/task-modular.json
                     
-                    # python eliozo_client.py get-classifiers --reference ../tests/master-demo/task-modular.json
+                    ${env.PYTHON_PATH} eliozo_client.py get-classifiers --reference ../tests/master-demo/task-modular.json
 
-                    # python eliozo_client.py get-problems --worksheet ../tests/master-demo/worksheet-modular.json  --reference ../tests/master-demo/task-modular.json
+                    ${env.PYTHON_PATH} eliozo_client.py get-problems --worksheet ../tests/master-demo/worksheet-modular.json  --reference ../tests/master-demo/task-modular.json
 
-                    # python eliozo_client.py convert-worksheet ../tests/master-demo/worksheet-modular.json ../tests/master-demo/worksheet-modular.rst --reference ../tests/master-demo/task-modular.json
+                    ${env.PYTHON_PATH} eliozo_client.py convert-worksheet ../tests/master-demo/worksheet-modular.json ../tests/master-demo/worksheet-modular.rst --reference ../tests/master-demo/task-modular.json
                     
-                    # python eliozo_client.py convert-worksheet ../tests/master-demo/worksheet-modular.rst ../tests/master-demo/worksheet-modular.docx --reference ../tests/master-demo/task-modular.json
+                    ${env.PYTHON_PATH} eliozo_client.py convert-worksheet ../tests/master-demo/worksheet-modular.rst ../tests/master-demo/worksheet-modular.docx --reference ../tests/master-demo/task-modular.json
                     """
                     echo("End: Creating a worksheet")
                 }
