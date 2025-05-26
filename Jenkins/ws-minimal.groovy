756e30a0b01b84df1f5e3c140daf4f1d8a4483e0
Kalvis Apsitis <kalvis.apsitis@zimperium.com>
2025-05-27 01:14:44 +0300

    abc

--- Jenkins/ws-minimal.groovy
+++ Jenkins/ws-minimal.groovy
@@ -43,7 +43,22 @@ pipeline {
         stage('Main') {
             steps {
                 script {
-                    echo("HELLO Main!")
+                    echo("Start: Creating a worksheet")
+                    writeFile(file: "query.txt", text: params.USER_QUERY, encoding: "UTF-8")
+                    sh """
+                    cd scripts
+                    export PYTHONPATH=".."
+                    python eliozo_client.py create-task --query ../tests/master-demo/query-modular.txt --reference ../tests/master-demo/task-modular.json
+
+                    # python eliozo_client.py get-classifiers --reference ../tests/master-demo/task-modular.json
+
+                    # python eliozo_client.py get-problems --worksheet ../tests/master-demo/worksheet-modular.json  --reference ../tests/master-demo/task-modular.json
+
+                    # python eliozo_client.py convert-worksheet ../tests/master-demo/worksheet-modular.json ../tests/master-demo/worksheet-modular.rst --reference ../tests/master-demo/task-modular.json
+                    
+                    # python eliozo_client.py convert-worksheet ../tests/master-demo/worksheet-modular.rst ../tests/master-demo/worksheet-modular.docx --reference ../tests/master-demo/task-modular.json
+                    """
+                    echo("End: Creating a worksheet")
                 }
             }
         }
