0e211a028a765d67cbead1711372813da000ab90
Kalvis Apsitis <kalvis.apsitis@zimperium.com>
2025-12-31 23:30:28 +0200

    math edu reports

---
+++ Jenkins/stat-math-education.groovy
@@ -0,0 +1,44 @@
+pipeline {
+    agent any
+    
+    environment {
+        PYTHON_PATH = '/opt/myenv/bin/python'
+    }
+    
+    stages {
+        stage('Checkout') {
+            steps {
+                cleanWs()
+                script {
+                    if (!env.PYTHON_PATH) {
+                        error "Environment variable PYTHON_PATH is not set."
+                    }
+                    
+                    echo "env.NODE_NAME=${env.NODE_NAME}"
+                    
+                    checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: "*/master"]],
+                        userRemoteConfigs: [[
+                            url: 'https://github.com/kapsitis/stat-math-education.git',
+                            credentialsId: 'github-pat-stat-math'
+                        ]],
+                        extensions: [
+                            [$class: 'RelativeTargetDirectory', relativeTargetDir: 'stat-math-education']
+                        ]
+                    ])
+
+                    echo "(1-a) End: Copy workspace for BRANCH_OR_TAG=${params.BRANCH_OR_TAG}"
+                }
+            }
+        }
+
+        stage('Run RScript') {
+            steps {
+                script {
+                    echo "Start: Run RScript"
+                }
+            }
+        }
+    }
+}
